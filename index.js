e2f5f6431e2e5399fb41e612f0837e490c197b34
James Kaviyil Jose <jamesjose03@gmail.com>
2019-10-06 19:51:39 +0530

    Added -u flag to CLI :tada:

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const mycli = require('commander')
-mycli.action(() => {
-    console.log('Hello World')
+
+const reply = word => {
+    console.log("Reply: " + word);
+}
+
+const collect = (val,arr) => {
+    arr.push(val);
+    return arr;
+}
+mycli
+.option('-u, --username <name>', `specify the user's name`)
+.option('-s, --silent', `disable output`)
+.action(() => {
+    if(!mycli.silent){
+        const name =  `Hello ${
+            typeof mycli.username === 'string' ? mycli.username : 'world'
+        }`
+        reply(name);
+    }
 })
 mycli.parse(process.argv)
